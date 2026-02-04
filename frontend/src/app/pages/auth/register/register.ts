76c60ee1d553e51f6389b915e8491bbf3dd6ac1f
srijachava <chsrijachowdary@gmail.com>
2026-02-04 17:20:20 -0500

    Update register.ts

--- frontend/src/app/pages/auth/register/register.ts
+++ frontend/src/app/pages/auth/register/register.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
